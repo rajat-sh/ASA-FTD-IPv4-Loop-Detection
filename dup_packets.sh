634bc108653c1ae6a320d4bb3faaf9de60aa225a
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2025-12-11 10:39:16 +0100

    Update dup_packets.sh

--- dup_packets.sh
+++ dup_packets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#version 1
-#This is a script to detect duplicate ipv4 packets from pcap files or "show capture capturename detail" output from ASA/FTD.
+# version 2
+# This is a script to detect duplicate IPv4 packets from pcap files or
+# "show capture capname detail" output from ASA/FTD.
 
 # Argument validation check
 if [ "$#" -ne 2 ]; then
@@ -9,7 +10,7 @@ if [ "$#" -ne 2 ]; then
     echo "For text Usage: $0 <text> <filename or path>"
     echo "Example for pcap: $0 pcap abc.pcap"
     echo "Example for text: $0 text abc.txt"
-    echo "Text file will be output of /show capture "capname" detail/ from ASA/FTD"
+    echo "Text file will be output of /show capture \"capname\" detail/ from ASA/FTD"
     echo ""
     exit 1
 fi
@@ -19,97 +20,124 @@ E_NOFILE=91           # Packet capture file does not exist.
 E_NOTEXTFILE=92       # File does not look like text file.
 E_NOCAPTUREFILE=93    # File does not look like capture file.
 
-#Parameters entered by user
-
+# Parameters entered by user
 INPUT=$1
 filename=$2
 
+# Check if capture/text file exists
+if [ ! -f "$filename" ]; then
+    echo "Capture File not found"
+    exit $E_NOFILE
+fi
 
-if [ ! -f $filename ]
-   then
-	   echo "Capture File not found"
-	   exit $E_NOFILE
-fi # Check if capture/text file exists.
-
-case $INPUT in
+case "$INPUT" in
 pcap)
-	if file "$filename" | grep -q "pcap capture file"; then
-    		:
-	else
-    		echo "File does not look like capture file"
-		exit $E_NOCAPTUREFILE
-	fi
-	tshark -r $filename -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf  | 
-	awk '! /0x00000000/ {
-	if($4==0){
-		str=$1"<------>"$2"-----------"$3
-		$0=str
-		a[$0]++}
-        }	
-	END{
-	printf "\n%s\n", "Potential duplicate packets"
-	printf "%s\t%s\t   %s\t%s\n","Count","Source IP","Destination IP","IP Identification"
-	for(x in a)
-	$0= a[x]" "x
-        counter=0	
-	if($1 != 1){ 
-	    printf "%s\t%s\n" ,$1,$2
-	    counter = counter + 1
-	}
+    # Basic file type check for pcap
+    if file "$filename" | grep -qi "pcap"; then
+        :
+    else
+        echo "File does not look like capture file"
+        exit $E_NOCAPTUREFILE
+    fi
 
-	if(counter == 0){
-		printf "\n%s\n","No looping packets found"
-	}
-	
+    tshark -r "$filename" -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf |
+    awk '
+        # Exclude IP ID 0x00000000 and only consider non-fragmented packets (MF=0)
+        ! /0x00000000/ {
+            if ($4 == 0) {
+                # Build key: "src<------>dst-----------id"
+                str = $1 "<------>" $2 "-----------" $3
+                a[str]++
+            }
+        }
+        END {
+            printf "\n%s\n", "Potential duplicate packets"
+            printf "%s\t%s\t   %s\t%s\n", "Count", "Source IP", "Destination IP", "IP Identification"
+
+            counter = 0
+            for (x in a) {
+                if (a[x] > 1) {
+                    # x is "src<------>dst-----------id"
+                    split(x, parts, "-----------")
+                    split(parts[1], sd, "<------>")
+                    src = sd[1]
+                    dst = sd[2]
+                    ipid = parts[2]
+                    printf "%d\t%s\t%s\t%s\n", a[x], src, dst, ipid
+                    counter++
+                }
+            }
+
+            if (counter == 0) {
+                printf "\n%s\n", "No looping packets found"
+            }
         }'
     ;;
 text)
-	if file "$filename" | grep -q text$; then
-    		:
-	else
-    		echo "File does not look like text file"
-		exit $E_NOTEXTFILE
-	fi
-	awk ' BEGIN {
-	num_iter = 0
-	}
+    # Basic file type check for text
+    if file "$filename" | grep -q text$; then
+        :
+    else
+        echo "File does not look like text file"
+        exit $E_NOTEXTFILE
+    fi
 
-	/ttl/ && !/id 0/ && !/frag/ {
-	str1=$1" <----> "$3"----- "$NF
-	gsub(")", "", str1)
-	gsub(":", "", str1)
-	gsub("\\.", " ", str1)
-	$0=str1
-	$0=$1"."$2"."$3"."$4"----------"$7"."$8"."$9"."$10"<--------->"$NF
-	a[$0]++
-	num_iter++
-	
-        }	
-        END{
-	if(num_iter == 0){
-		printf "\n%s\n", "No Looping Packets Found"
-		exit
-	}
-	printf "\n%s\n", "Potential duplicate packets"
-	printf "%s\t%s\t   %s\t%s\n","Count","Source IP","Destination IP","IP Identification"
-	for(x in a){
-		$0= a[x]" "x
-	}
-        counter=0
-	
-	if($1 != 1 ){
-	        	
-	    	printf "%s\t%s\n" ,$1,$2
-	    	counter = counter + 1
-	}
+    awk '
+        BEGIN {
+            num_iter = 0
+        }
 
-	if(counter == 0){
-		printf "\n%s\n","No looping packets found"
-	}
-	}' $filename 
+        # Typical ASA/FTD line with ttl, but no "id 0" and no "frag"
+        /ttl/ && !/id 0/ && !/frag/ {
+            # Example original fields:
+            # 1=src_ip, 3=dst_ip, NF=... (IP id at the end usually "id 0x1234")
+            str1 = $1" <----> "$3"----- "$NF
+            gsub(")", "", str1)
+            gsub(":", "", str1)
+            gsub("\\.", " ", str1)
+
+            # Overwrite $0 so we can break out src/dst octets
+            $0 = str1
+            # Now fields are: src1 src2 src3 src4 <----> dst1 dst2 dst3 dst4 ----- id
+            # We rebuild: "src----------dst<--------->id"
+            src = $1"."$2"."$3"."$4
+            dst = $7"."$8"."$9"."$10
+            ipid = $NF
+            key = src "----------" dst "<--------->" ipid
+
+            a[key]++
+            num_iter++
+        }
+
+        END {
+            if (num_iter == 0) {
+                printf "\n%s\n", "No Looping Packets Found"
+                exit
+            }
+
+            printf "\n%s\n", "Potential duplicate packets"
+            printf "%s\t%s\t   %s\t%s\n", "Count", "Source IP", "Destination IP", "IP Identification"
+
+            counter = 0
+            for (x in a) {
+                if (a[x] > 1) {
+                    # x is "src----------dst<--------->id"
+                    split(x, parts, "<--------->")
+                    ipid = parts[2]
+                    split(parts[1], sd, "----------")
+                    src = sd[1]
+                    dst = sd[2]
+                    printf "%d\t%s\t%s\t%s\n", a[x], src, dst, ipid
+                    counter++
+                }
+            }
+
+            if (counter == 0) {
+                printf "\n%s\n", "No looping packets found"
+            }
+        }' "$filename"
     ;;
 *)
-    echo "Enter a valid choice"	
+    echo "Enter a valid choice (pcap | text)"
     ;;
 esac
-
