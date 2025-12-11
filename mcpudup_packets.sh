f6576961dd22716acd43e3c11a9847a2ab3f0225
rajat-sh <39512057+rajat-sh@users.noreply.github.com>
2025-12-11 10:48:07 +0100

    Update mcpudup_packets.sh

--- mcpudup_packets.sh
+++ mcpudup_packets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#version 1
+#version 2
 #This is a script to detect duplicate packets
 
-#Exit Codes
+# Exit Codes
 E_NOFILE=91           # Packet capture file does not exist.
 E_NOTEXTFILE=92       # File does not look like text file.
 E_NOCAPTUREFILE=93    # File does not look like capture file.
@@ -19,143 +19,163 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-#Parameters entered by user
-
+# Parameters entered by user
 INPUT=$1
 filename=$2
 
+# Check if capture/text file exists.
+if [ ! -f "$filename" ]; then
+    echo "Capture File not found"
+    exit $E_NOFILE
+fi
 
-if [ ! -f $filename ]
-   then
-	   echo "Capture File not found"
-	   exit $E_NOFILE
-fi # Check if capture/text file exists.
-
-
-case $INPUT in
+case "$INPUT" in
 pcap)
-	if file "$filename" | grep -q "pcap capture file"; then
-    		:
-	else
-    		echo "File does not look like capture file"
-		exit $E_NOCAPTUREFILE
-	fi
-	myfilesize=$(wc -c "$filename" | awk '{print $1}')
-	if [[ $myfilesize -gt 100000000 ]]
-	then
-			let newfilesize="$myfilesize"/4000000
-			prefix=temp
-			suffix=$(date +%s)  # The "+%s" option to 'date' is GNU-specific.
-			capturefilename=$prefix.$suffix.PCAP
-	
-			tcpdump -r $filename -w /tmp/$capturefilename -C $newfilesize
-			cp /tmp/$capturefilename* .
-			rm /tmp/$capturefilename*
-			filelist=$prefix.$suffix.TEXT
-			ls $capturefilename* >> $prefix.$suffix.TEXT
-			parallel -j 4  tshark -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf -r {} < $prefix.$suffix.TEXT |
-			#tshark -r $filename -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf  | 
-			awk '! /0x00000000/ {
-			if($4==0){
-				str=$1"<------>"$2"-----------"$3
-				$0=str
-				a[$0]++}
-        		}	
-			END{
-			printf "\n%s\n", "Potential duplicate packets"
-			printf "%s\t%s\t   %s\t%s\n","Count","Source IP","Destination IP","IP Identification"
-			for(x in a)
-			$0= a[x]" "x
-        		counter=0	
-			if($1 != 1){ 
-	    		printf "%s\t%s\n" ,$1,$2
-	    		counter = counter + 1
-			}
+    if file "$filename" | grep -q "pcap capture file"; then
+        :
+    else
+        echo "File does not look like capture file"
+        exit $E_NOCAPTUREFILE
+    fi
 
-			if(counter == 0){
-				printf "\n%s\n","No looping packets found"
-			}
-	
-	
-        		}'
-	
-			rm $capturefilename* $prefix.$suffix.TEXT
-		else
-			tshark -r $filename -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf  | 
-			awk '! /0x00000000/ {
-			if($4==0){
-				str=$1"<------>"$2"-----------"$3
-				$0=str
-				a[$0]++}
-        		}	
-			END{
-			printf "\n%s\n", "Potential duplicate packets"
-			printf "%s\t%s\t   %s\t%s\n","Count","Source IP","Destination IP","IP Identification"
-			for(x in a)
-			$0= a[x]" "x
-        		counter=0	
-			if($1 != 1){ 
-	    			printf "%s\t%s\n" ,$1,$2
-	    			counter = counter + 1
-			}
+    myfilesize=$(wc -c "$filename" | awk '{print $1}')
+    # Use single-bracket test for portability
+    if [ "$myfilesize" -gt 100000000 ]; then
+        # Large file path: split, then parallel tshark
+        newfilesize=$(( myfilesize / 4000000 ))
+        prefix=temp
+        suffix=$(date +%s)
+        capturefilename=$prefix.$suffix.PCAP
 
-			if(counter == 0){
-			printf "\n%s\n","No looping packets found"
-			}
-	
-        		}'
+        tcpdump -r "$filename" -w /tmp/"$capturefilename" -C "$newfilesize"
+        cp /tmp/"$capturefilename"* .
+        rm /tmp/"$capturefilename"*
+        ls "$capturefilename"* > "$prefix.$suffix.TEXT"
 
-		fi
-	
+        parallel -j 4 tshark -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf -r {} :::: "$prefix.$suffix.TEXT" |
+        awk '
+            ! /0x00000000/ {
+                if ($4 == 0) {
+                    key = $1 "<------>" $2 "-----------" $3
+                    a[key]++
+                }
+            }
+            END {
+                printf "\n%-8s %-15s %-15s %-18s\n", "Count", "Source IP", "Destination IP", "IP Identification"
+                printf "%-8s %-15s %-15s %-18s\n", "-----", "---------------", "---------------", "------------------"
+
+                counter = 0
+                for (x in a) {
+                    if (a[x] > 1) {
+                        # x format: src<------>dst-----------id
+                        split(x, parts, "-----------")
+                        split(parts[1], sd, "<------>")
+                        src  = sd[1]
+                        dst  = sd[2]
+                        ipid = parts[2]
+                        printf "%-8d %-15s %-15s %-18s\n", a[x], src, dst, ipid
+                        counter++
+                    }
+                }
+                if (counter == 0) {
+                    printf "\nNo looping packets found\n"
+                }
+            }'
+
+        rm "$capturefilename"* "$prefix.$suffix.TEXT"
+    else
+        # Small/normal file path: single tshark
+        tshark -r "$filename" -T fields -e ip.src -e ip.dst -e ip.id -e ip.flags.mf |
+        awk '
+            ! /0x00000000/ {
+                if ($4 == 0) {
+                    key = $1 "<------>" $2 "-----------" $3
+                    a[key]++
+                }
+            }
+            END {
+                printf "\n%-8s %-15s %-15s %-18s\n", "Count", "Source IP", "Destination IP", "IP Identification"
+                printf "%-8s %-15s %-15s %-18s\n", "-----", "---------------", "---------------", "------------------"
+
+                counter = 0
+                for (x in a) {
+                    if (a[x] > 1) {
+                        # x format: src<------>dst-----------id
+                        split(x, parts, "-----------")
+                        split(parts[1], sd, "<------>")
+                        src  = sd[1]
+                        dst  = sd[2]
+                        ipid = parts[2]
+                        printf "%-8d %-15s %-15s %-18s\n", a[x], src, dst, ipid
+                        counter++
+                    }
+                }
+                if (counter == 0) {
+                    printf "\nNo looping packets found\n"
+                }
+            }'
+    fi
     ;;
+
 text)
-	if file "$filename" | grep -q text$; then
-    		:
-	else
-    		echo "File does not look like text file"
-		exit $E_NOTEXTFILE
-	fi
+    if file "$filename" | grep -q text$; then
+        :
+    else
+        echo "File does not look like text file"
+        exit $E_NOTEXTFILE
+    fi
 
-	awk ' BEGIN {
-	num_iter = 0
-	}
+    awk '
+        BEGIN {
+            num_iter = 0
+        }
 
-	/ttl/ && !/id 0/ && !/frag/ {
-	str1=$1" <----> "$3"----- "$NF
-	gsub(")", "", str1)
-	gsub(":", "", str1)
-	gsub("\\.", " ", str1)
-	$0=str1
-	$0=$1"."$2"."$3"."$4"----------"$7"."$8"."$9"."$10"<--------->"$NF
-	a[$0]++
-	num_iter++
-	
-        }	
-        END{
-	if(num_iter == 0){
-		printf "\n%s\n", "No Looping Packets Found"
-		exit
-	}
-	printf "\n%s\n", "Potential duplicate packets"
-	printf "%s\t%s\t   %s\t%s\n","Count","Source IP","Destination IP","IP Identification"
-	for(x in a){
-		$0= a[x]" "x
-	}
-        counter=0
-	
-	if($1 != 1 ){
-	        	
-	    	printf "%s\t%s\n" ,$1,$2
-	    	counter = counter + 1
-	}
+        /ttl/ && !/id 0/ && !/frag/ {
+            str1 = $1" <----> "$3"----- "$NF
+            gsub(")", "", str1)
+            gsub(":", "", str1)
+            gsub("\\.", " ", str1)
+            $0 = str1
 
-	if(counter == 0){
-		printf "\n%s\n","No looping packets found"
-	}
-	}' $filename 
+            # Now fields are: src1 src2 src3 src4 <----> dst1 dst2 dst3 dst4 ----- id
+            src  = $1"."$2"."$3"."$4
+            dst  = $7"."$8"."$9"."$10
+            ipid = $NF
+            key  = src"----------"dst"<--------->"ipid
+
+            a[key]++
+            num_iter++
+        }
+
+        END {
+            if (num_iter == 0) {
+                printf "\nNo Looping Packets Found\n"
+                exit
+            }
+
+            printf "\n%-8s %-15s %-15s %-18s\n", "Count", "Source IP", "Destination IP", "IP Identification"
+            printf "%-8s %-15s %-15s %-18s\n", "-----", "---------------", "---------------", "------------------"
+
+            counter = 0
+            for (x in a) {
+                if (a[x] > 1) {
+                    # x format: src----------dst<--------->id
+                    split(x, parts, "<--------->")
+                    ipid = parts[2]
+                    split(parts[1], sd, "----------")
+                    src = sd[1]
+                    dst = sd[2]
+                    printf "%-8d %-15s %-15s %-18s\n", a[x], src, dst, ipid
+                    counter++
+                }
+            }
+
+            if (counter == 0) {
+                printf "\nNo looping packets found\n"
+            }
+        }' "$filename"
     ;;
 *)
-    echo "Enter a valid choice"	
+    echo "Enter a valid choice"
     ;;
 esac
-
